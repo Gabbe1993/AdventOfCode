992ed18800bfde0ae588850474ce1fbf92291b65
gabbe1993 <gabriel.vilen@gmail.com>
2015-12-17 20:30:00 +0100

    Day 2, part two

--- src/days/Day2.java
+++ src/days/Day2.java
@@ -11,25 +11,26 @@ import java.util.Scanner;
  */
 public class Day2 implements Day {
 
-    int h, l, w, total, slack, s1, s2, s3;
+    private int total;
+    private int s[] = new int[2];
 
     @Override
     public void partOne(String input) {
         String[] sizes = input.split("x");
-        l = Integer.parseInt(sizes[0]);
-        w = Integer.parseInt(sizes[1]);
-        h = Integer.parseInt(sizes[2]);
+        int l = Integer.parseInt(sizes[0]);
+        int w = Integer.parseInt(sizes[1]);
+        int h = Integer.parseInt(sizes[2]);
 
-        s1 = l * w;
-        s2 = w * h;
-        s3 = h * l;
-
+        int s1 = l * w;
+        int s2 = w * h;
+        int s3 = h * l;
+        int slack;
 
         if (s1 <= s2 && s1 <= s3) {
             slack = s1;
         } else if (s2 <= s3 && s2 <= s1) {
             slack = s2;
-        } else if (s3 <= s1 && s3 <= s2) {
+        } else {
             slack = s3;
         }
         total += (2 * s1) + (2 * s2) + (2 * s3) + slack;
@@ -37,8 +38,22 @@ public class Day2 implements Day {
 
     @Override
     public void partTwo(String input) {
-        System.out.println("test");
+        String[] sizes = input.split("x");
+        int l = Integer.parseInt(sizes[0]);
+        int w = Integer.parseInt(sizes[1]);
+        int h = Integer.parseInt(sizes[2]);
 
+        if(l <= w || l <= h) {
+            s[0] = l;
+            s[1] = (w <= h) ? w : h;
+        } else {
+            s[0] = w;
+            s[1] = h;
+        }
+        int ribbon = 2 * s[0] + 2 * s[1];
+        int bow = l * w * h;
+
+        total += ribbon + bow;
     }
 
     @Override
@@ -46,10 +61,8 @@ public class Day2 implements Day {
         try {
             Scanner scanner = new Scanner(new File(ReadFromFile.PATH + day + ".txt"));
             while (scanner.hasNextLine()) {
-                partOne(scanner.next());
+                partTwo(scanner.next());
             }
-            //partOne("2x3x4");
-            //partOne("1x1x10");
             System.out.println("Total: " + total);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
